51e08b1e3550e980d1bbb5d68e153d604eb829b1
TimeLordRaps <tyler.roost@gmail.com>
2024-04-07 01:26:00 -0700

    Added file_storage_path env var

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        FILE_STORAGE_PATH: process.env.FILE_STORAGE_PATH,
+    },
+};
+
 
 export default nextConfig;
